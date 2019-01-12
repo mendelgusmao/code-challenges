3dcf31939f019101cd6ecabfdbe8a6486ce4702f
MendelGusmao <mendelsongusmao@gmail.com>
2019-01-12 21:52:56 -0200

    Extract common middleware

--- backend/router/router.go
+++ backend/router/router.go
@@ -2,13 +2,15 @@ package router
 
 import (
 	"bitbucket.org/mendelgusmao/me_gu/backend/middleware"
+	common "bitbucket.org/mendelgusmao/me_gu/common/middleware"
+
 	"github.com/gorilla/mux"
 )
 
 var Router = mux.NewRouter()
 
 func init() {
-	Router.Use(middleware.ContentType("application/json; charset=utf-8"))
+	Router.Use(common.ContentType("application/json; charset=utf-8"))
 	Router.Use(middleware.Database)
-	Router.Use(middleware.Logging)
+	Router.Use(common.Logging)
 }
--- frontend/router/router.go
+++ frontend/router/router.go
@@ -1,8 +1,16 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	common "bitbucket.org/mendelgusmao/me_gu/common/middleware"
+	"github.com/gorilla/mux"
+)
 
 var (
 	Router = mux.NewRouter()
 	AJAX   = Router.PathPrefix("/ajax").Subrouter()
 )
+
+func init() {
+	Router.Use(common.Logging)
+	AJAX.Use(common.ContentType("application/json; charset=utf-8"))
+}
