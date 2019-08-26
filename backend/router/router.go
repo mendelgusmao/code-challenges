7f4410872801c72eb3ce905602d9f1d6adef0a2b
MendelGusmao <mendelsongusmao@gmail.com>
2019-08-26 19:19:05 -0300

    Implement ContentType middleware (outputs application/json for all requests)

--- backend/router/router.go
+++ backend/router/router.go
@@ -1,7 +1,13 @@
 package router
 
 import (
+	"gitlab.com/cabify-challenge/car-pooling-challenge-mendelgusmao/backend/middleware"
+
 	"github.com/gorilla/mux"
 )
 
 var Router = mux.NewRouter()
+
+func init() {
+	Router.Use(middleware.ContentType("application/json; charset=utf-8"))
+}
