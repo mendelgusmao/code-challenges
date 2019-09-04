68fc5ae53cc41e755abb647f699afce37c855039
MendelGusmao <mendelsongusmao@gmail.com>
2019-09-04 17:36:13 -0300

    Define the Product objects

---
+++ backend/services/product.go
@@ -0,0 +1,26 @@
+package services
+
+type Product struct {
+	Name   string  `json:"name"`
+	Height float64 `json:"height"`
+	Length float64 `json:"length"`
+	Width  float64 `json:"width"`
+	Weight float64 `json:"weight"`
+	Price  float64 `json:"price"`
+}
+
+type TaxedProduct struct {
+	Product
+	Tax float64 `json:"tax,omitempty"`
+}
+
+func (p Product) toMap() map[string]interface{} {
+	return map[string]interface{}{
+		"name":   p.Name,
+		"height": p.Height,
+		"length": p.Length,
+		"width":  p.Width,
+		"weight": p.Weight,
+		"price":  p.Price,
+	}
+}
